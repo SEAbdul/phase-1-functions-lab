cae497df33afb6675a3cf563e1dc7bb5e887844e
Abdul Shakur Mohammed <abdshakurmohammed@gmail.com>
2023-03-29 22:05:41 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(distance) {
+    console.log(42,num2)
+    return (num2-42)
+  }
+  function distanceFromHqInFeet(distance) {
+    distanceFromHqInBlocks(feet);
+    console.log(distance,feet)
+    return (distance*264)
+  }
+  function distanceTravelledInFeet(start, destination) {
+    return ((destination-start)*264)
+  }
+  function calculatesFarePrice(start, destination) {
+    return ((destination-start-(400)*2))
+    if{(destination-start-(400))>2500)
+    return'cannot travel that far' }
+  }
